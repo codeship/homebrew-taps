08344012bef25be43b195d84c8299167d7973c77
Mark Phelps <mark.aaron.phelps@gmail.com>
2017-12-08 15:33:58 -0500

    cask cop

--- Casks/jet.rb
+++ Casks/jet.rb
@@ -2,6 +2,7 @@ cask 'jet' do
   version '1.19.24'
   sha256 '718705eab04e0f355205e7aaa4f5f1a6d7aa5a2dcb7dfcac87f33690297a5fc6'
 
+  # s3.amazonaws.com/codeship-jet-releases was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/codeship-jet-releases/#{version}/jet-darwin_amd64_#{version}.tar.gz"
   name 'Codeship Jet'
   homepage 'https://documentation.codeship.com/pro/builds-and-configuration/cli/'
---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'rubocop', '~> 0.51.0', require: false
+gem 'rubocop-cask'
