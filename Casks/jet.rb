703defead26868826fb79109d1128d7e5c48bfce
codeship <dev@codeship.com>
2018-01-30 19:28:02 +0000

    Updating jet Cask to version 2.3.1

--- Casks/jet.rb
+++ Casks/jet.rb
@@ -1,6 +1,6 @@
 cask 'jet' do
-  version '2.3.0'
-  sha256 'faf3dde68ada87c225ce79f8c44c87d93d9987b9e40375c2ee5bce55b65e7ec3'
+  version '2.3.1'
+  sha256 '4bb1f1892a29582ac61f770ca89975462bc3c30e00d89d032096da600897dbff'
 
   # s3.amazonaws.com/codeship-jet-releases was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/codeship-jet-releases/#{version}/jet-darwin_amd64_#{version}.tar.gz"
