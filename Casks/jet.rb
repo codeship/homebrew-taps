49b6b371e3e7473639c94c47aa9f08a37244a2c5
Brett Buddin <b@buddin.us>
2018-10-02 12:57:59 -0400

    Manually update SHA 256 to correct value

--- Casks/jet.rb
+++ Casks/jet.rb
@@ -1,6 +1,6 @@
 cask 'jet' do
   version '2.8.0'
-  sha256 '5ee65fa733af68f554919ec7c840f98f678d5516a24220ec7d3a32d95ebdcbf4'
+  sha256 'cd176f11180fa4e21a76c8314e790a8a5a9477f4d064838266b68c33cc0eb8ac'
 
   # s3.amazonaws.com/codeship-jet-releases was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/codeship-jet-releases/#{version}/jet-darwin_amd64_#{version}.tar.gz"
